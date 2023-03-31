925cb0828f87a3d5f6edcfd663340c6a7202aaa3
Nino van Hooff <nino@q42.nl>
2023-03-31 14:16:06 +0200

    CLEANUP sample
    and add wall time, iteration count + install tools

--- Source/main.lua
+++ Source/main.lua
@@ -1,6 +1,9 @@
-
+local currentTime <const> = playdate.sound.getCurrentTime
 local snd = playdate.sound
 local gfx = playdate.graphics
+
+-- iterations per second
+playdate.display.setRefreshRate(2)
 
 s = snd.sequence.new('giveyouup.mid')
 
@@ -14,77 +17,32 @@ function newsynth()
 	return s
 end
 
-function drumsynth(path, code)
-	local sample = snd.sample.new(path)
-	local s = snd.synth.new(sample)
-	s:setVolume(0.5)
-	return s
-end
-
-function newinst(n)
+function newinst()
+	local synth = newsynth()
 	local inst = snd.instrument.new()
-	for i=1,n do
-		inst:addVoice(newsynth())
-	end
-	return inst
-end
-
-function druminst()
-	local inst = snd.instrument.new()
-	inst:addVoice(drumsynth("drums/kick"), 35)
-	inst:addVoice(drumsynth("drums/kick"), 36)
-	inst:addVoice(drumsynth("drums/snare"), 38)
-	inst:addVoice(drumsynth("drums/clap"), 39)
-	inst:addVoice(drumsynth("drums/tom-low"), 41)
-	inst:addVoice(drumsynth("drums/tom-low"), 43)
-	inst:addVoice(drumsynth("drums/tom-mid"), 45)
-	inst:addVoice(drumsynth("drums/tom-mid"), 47)
-	inst:addVoice(drumsynth("drums/tom-hi"), 48)
-	inst:addVoice(drumsynth("drums/tom-hi"), 50)
-	inst:addVoice(drumsynth("drums/hh-closed"), 42)
-	inst:addVoice(drumsynth("drums/hh-closed"), 44)
-	inst:addVoice(drumsynth("drums/hh-open"), 46)
-	inst:addVoice(drumsynth("drums/cymbal-crash"), 49)
-	inst:addVoice(drumsynth("drums/cymbal-ride"), 51)
-	inst:addVoice(drumsynth("drums/cowbell"), 56)
-	inst:addVoice(drumsynth("drums/clav"), 75)
-	return inst
+	inst:addVoice(synth)
+	return inst, synth
 end
 
 local ntracks = s:getTrackCount()
-local active = {}
-local poly = 0
-local tracks = {}
 
-for i=1,ntracks do
-	local track = s:getTrackAtIndex(i)
-	if track ~= nil then
-		local n = track:getPolyphony(i)
-		if n > 0 then active[#active+1] = i end
-		if n > poly then poly = n end
-		print("track "..i.." has polyphony "..n)
-	
-		if i == 10 then
-			track:setInstrument(druminst(n))
-		else
-			track:setInstrument(newinst(n))
-		end
-	end
-end
-
-s:play()
+local iterations = 0
 
 function playdate.update()
 	gfx.clear(gfx.kColorWhite)
 	gfx.setColor(gfx.kColorBlack)
 	
-	for i=1,#active do
+	for i=1,ntracks do
 		local track = s:getTrackAtIndex(i)
-		local n = track:getNotesActive(active[i])
-		gfx.fillRect(400*(i-1)/#active, 240*(1-n/poly), 400/#active, 240)
-
-		track:setInstrument(newinst(1))
+		local newInst, newSynth = newinst(1)
+		print("newSynth", newSynth)
+		track:setInstrument(newInst)
 		s:play()
 	end
-	
+
+	local iterationText = currentTime() .. " iteration " .. iterations
+	gfx.drawText(iterationText,100, 100)
+	print(iterationText)
+	playdate.drawFPS()
+	iterations = iterations + 1
 end
